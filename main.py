c7662d0292488e54c852deb2b18a24bdd3c94ade
Syam Babu <>
2023-03-17 13:12:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+l="reverse_string"
+u=l.upper()
+print(u)
+v=u.capitalize()
+print(v)
+print(l.find("str"))
+q=l.replace("reverse","normal")
+print(q)
+#strings are immutable we can change the whole value
