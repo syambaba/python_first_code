5ce436e780f55aa8488d8a15968b084f379f74e5
Syam Babu <97274517+syambaba@users.noreply.github.com>
2023-03-20 04:05:43 +0000

    find_dup exercise updated

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
-]
-for row in picture:
-  for elem in row:
-    if(elem==0):
-      print(" ",end="")
-    else:
-      print("*",end="")
-  print("\n")
-  
+l=["a","b","c","b","d","n","n"]
+is_dup=[]
+for i in range(len(l)): #iterate all items
+  for j in range(i+1,len(l)):#every item is checked for duplicacy
+    if(l[i]==l[j]):
+      is_dup.append(l[i]) #if duplicate break the loop go for next element check
+      break
+print(is_dup)
+#another method
+some_list=["a","b","c","b","d","n","n"]
+is_dup=[]
+for item in some_list:
+  if some_list.count(item)>1:
+    if item not in is_dup:
+      is_dup.append(item)
+print(is_dup)
