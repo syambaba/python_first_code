ba06123428083e5efda7e40e09f0278019af7131
Syam Babu <97274517+syambaba@users.noreply.github.com>
2023-03-29 10:41:43 +0000

    dict_methods_learn

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 l=[{"a":[1,2,3,4],"b":"hi",1:True},
-   {"c":[1,2,3,4],"d":"hii",1:True}
+   {"c":[1,2,3,4],"d":"hii","de":"baa",1:True}
   ]
-print(l[0][2])["a"])
+print(l[0]["a"][2])
+print(l[1].get("d"))
+print(l[1].get("de","hello"))#optional value to be return if not present
+print(l[1].update({"syam":"babu"}))
+print(l[1])
